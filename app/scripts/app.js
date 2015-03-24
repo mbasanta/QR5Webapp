5ca09a50b2334d358526259c20ead956cffbe73c
Matthew Basanta <matthew.basanta@gmail.com>
2015-03-24 11:01:20 -0400

    Initial Commit

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name qr5WebappApp
+ * @description
+ * # qr5WebappApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('qr5WebappApp', [
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
